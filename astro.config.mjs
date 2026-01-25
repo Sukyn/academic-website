4364fa595f46264540189a8e1df324cd2decc857
Colin Blake <cblake@ad.inria.fr>
2026-01-25 12:21:24 +0100

    Circuits fragments blog post

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://colin-blake.eu/",
-  integrations: [mdx()]
+  integrations: [
+    mdx({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
+  ],
 });
